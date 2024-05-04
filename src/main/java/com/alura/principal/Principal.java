cb4f847215f37a1fab2418d02d548a6d6508edca
HernanGoldy <hernanmdq@gmail.com>
2024-05-04 20:38:53 -0300

    Top 10 libros más descargados

--- src/main/java/com/alura/principal/Principal.java
+++ src/main/java/com/alura/principal/Principal.java
@@ -1,9 +1,11 @@
 package com.alura.principal;
 
 import com.alura.models.DatosGenerales;
+import com.alura.models.DatosLibro;
 import com.alura.services.ConsumoAPI;
 import com.alura.services.ConvierteDatosImp;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
@@ -18,5 +20,13 @@ public class Principal {
         System.out.println(json);
         DatosGenerales datos = conversor.obtenerDatos(json, DatosGenerales.class);
         System.out.println(datos);
+
+        // Top 10 libros más descargados
+        System.out.println("Los 10 libros más descargados son: ");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibro::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo())
+                .forEach(System.out::println);
     }
 }
